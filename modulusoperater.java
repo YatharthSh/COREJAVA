2b8d3eeb40dcfa3a4f2a78833580451370a2eb90
YatharthSh <44478305+YatharthSh@users.noreply.github.com>
2021-09-07 10:37:36 +0530

    Add files via upload

---
+++ modulusoperater.java
@@ -0,0 +1,12 @@
+
+public class modulusoperater {
+
+	public static void main(String[] args) {
+		int i=50;
+		double d= 32;
+		System.out.println("i mod 10 = "+i%10);
+		System.out.println("d mod 10 = "+d%10);
+
+	}
+
+}
